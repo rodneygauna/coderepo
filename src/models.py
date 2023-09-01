f9783dfa3080f73805ba62c086b9305b5cef04c1
Rodney Gauna <rodneygauna@gmail.com>
2023-09-01 20:48:33 +0000

    start of the icd10cm table

--- src/models.py
+++ src/models.py
@@ -51,3 +51,25 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         """Checks if the password is correct"""
         return check_password_hash(self.password_hash, password)
+
+
+# Model - ICD-10 Library
+class ICD10CM(db.Model):
+    """ICD-10-CM model"""
+
+    __tablename__ = "icd10cm"
+
+    # IDs
+    id = db.Column(db.Integer, primary_key=True)
+    # ICD-10-CM Library Year
+    library_year = db.Column(db.DateTime, nullable=False)
+    # ICD-10-CM Code
+    diagnosis_code = db.Column(db.String(10), nullable=False)
+    # ICD-10-CM Description
+    diagnosis_description = db.Column(db.String(255), nullable=False)
+    # ICD-10-CM Inclusion Terms
+    inclusion_term_1 = db.Column(db.String(255))
+    inclusion_term_2 = db.Column(db.String(255))
+    inclusion_term_3 = db.Column(db.String(255))
+    inclusion_term_4 = db.Column(db.String(255))
+    inclusion_term_5 = db.Column(db.String(255))
